1a898fd4c31e727b62911f91cfda22ff19c05e71
Dhruv Mittal <dhruv@MacBook-Air.local>
2020-06-14 10:10:24 -0700

    company creating ui implemented

--- reactapp/src/components/Home.js
+++ reactapp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Container, Card, CardBody, Button, Form, Input } from 'reactstrap'
+import {Container, Card, CardBody, Button, Form, Input, FormGroup, Label } from 'reactstrap'
 import { connect } from 'react-redux';
 import { getStock, saveStock, saveManyStocks } from '../nodeserverapi'
 import Plot from 'react-plotly.js'
@@ -8,29 +8,53 @@ import { signedInUserMstp, signedInUserMdtp, getUserToken } from '../redux/conta
 class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = { company: ["COF", "FB", "AMZN", "AAPL", "GOOGL"] }
+    this.state = { companies: [], companiesStr: '' }
+  }
+
+  onChangeCompaniesStr = (e) => {
+    console.log(e.target.value)
+    this.setState({ companiesStr: e.target.value })
+  }
+
+  addCompanies = (e) => {
+    e.preventDefault()
+    const { companiesStr } = this.state;
+
+    let companiesList = companiesStr.split(', ')
+
+    this.setState({ companiesStr: '', companies: companiesList })
   }
 
   updateStock = () => {
-    let { company } = this.state
+    let { companies } = this.state
 
-      getStock(getUserToken(), company, 
-        response => {
-          console.log('response->', response.data)
-        },
-        error => {
-          console.log(error.message)
-        }
-      )
+    getStock(getUserToken(), companies, 
+      response => {
+        console.log('response->', response.data)
+      },
+      error => {
+        console.log(error.message)
+      }
+    )
     
   }
 
   render() {
+    const { companiesStr } = this.state;
 
     return (
       <Container className='dashboard'>
         <Card>
           <CardBody>
+
+            <Form onSubmit={this.addCompanies}>
+              <FormGroup>
+                <Label>Add Companies</Label>
+              </FormGroup>
+              <FormGroup>
+                <Input name='companiesStr' value={companiesStr} onChange={this.onChangeCompaniesStr}/>
+              </FormGroup>
+            </Form>
 
             <p>Home</p>
             <Button size='sm' color='primary' onClick={this.updateStock}>Update Stocks</Button>
